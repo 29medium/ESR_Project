a9f050f0212720476b550ee221b6ecae7aec0031
Paulo Sousa <45037062+29medium@users.noreply.github.com>
2021-12-23 17:14:00 +0000

    Faltavam pacotes no server

--- TP3/src/ServerReceiverTCP.java
+++ TP3/src/ServerReceiverTCP.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ServerReceiverTCP implements Runnable{
     private ServerSocket ss;
@@ -35,6 +38,9 @@ public class ServerReceiverTCP implements Runnable{
                         Packet.send(out, new Packet(p.getDestination(), p.getSource(), 3, data.getBytes(StandardCharsets.UTF_8)));
                         Ott.changed = true;
                     }
+
+                } else if (p.getType() == 4) {
+                    Packet.send(out, new Packet(p.getDestination(), p.getSource(), 5, "1 null".getBytes(StandardCharsets.UTF_8)));
                 } else if (p.getType() == 6) {
                     at.addAddress(p.getSource());
                 } else if(p.getType() == 11) {
@@ -45,6 +51,23 @@ public class ServerReceiverTCP implements Runnable{
                     at.setStatus(p.getSource(), false, streamID);
                 } else if(p.getType() == 14) {
                     Ott.changed = true;
+                } else if (p.getType() == 16) {
+                    int hops = at.getHops() + 1;
+                    String msg = hops + " " + at.getSender();
+                    Packet.send(out, new Packet(p.getDestination(), p.getSource(), 17, msg.getBytes(StandardCharsets.UTF_8)));
+                } else if(p.getType() == 19) {
+                    Set<String> neighbours_temp = at.getNeighbourTemp();
+                    Set<String> nei = new TreeSet<>(List.of(new String(p.getData(), StandardCharsets.UTF_8).split(",")));
+
+                    for(String n : neighbours_temp) {
+                        for(String ne : nei) {
+                            if(n.equals(ne)) {
+                                at.removeNeighbourTemp(ne);
+                            }
+                        }
+                    }
+                } else if(p.getType() == 20) {
+                    at.addNeighbourTemp(p.getSource());
                 }
 
                 out.close();
